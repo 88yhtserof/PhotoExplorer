9f818c6071b2e48390fb579b86309171dc3c5d2a
88yhtserof <88yhtserofg@gmail.com>
2025-01-17 17:29:54 +0900

    [FEAT] ConfigurationVC 생성 #1

---
+++ PhotoExplorer/PhotoExplorer/ConfigurationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ConfigurationViewController.swift
+//  PhotoExplorer
+//
+//  Created by 임윤휘 on 1/17/25.
+//
+
+import UIKit
+
+/// A view controller that provides configuration methods and calls those methods at an appropriate times.
+class ConfigurationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+        configureHierarchy()
+        configureConstraints()
+    }
+    
+    func configureView(){}
+    
+    func configureHierarchy() {}
+    
+    func configureConstraints() {}
+}
