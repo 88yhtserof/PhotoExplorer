b46cc844978f9b005304e9a65987b57759c390ba
88yhtserof <88yhtserofg@gmail.com>
2025-01-19 01:51:33 +0900

    [FEAT] PhotoSearch: 좋아요 개수 표시 #1

--- PhotoExplorer/PhotoExplorer/PhotoCollectionViewCell.swift
+++ PhotoExplorer/PhotoExplorer/PhotoCollectionViewCell.swift
@@ -12,6 +12,8 @@ import Kingfisher
 final class PhotoCollectionViewCell: ConfigurationCollectionViewCell, ListConfigurable {
     
     private var photoImageView = UIImageView()
+    private var starButton = UIButton()
+    private var starButtonConfiguration = UIButton.Configuration.filled()
     
     static let identifier = String(describing: PhotoCollectionViewCell.self)
     private lazy var width: CGFloat = contentView.frame.width
@@ -24,19 +26,31 @@ final class PhotoCollectionViewCell: ConfigurationCollectionViewCell, ListConfig
     override func configureContentView() {
         photoImageView.backgroundColor = .systemGray6
         contentView.backgroundColor = .red
+        
+        starButtonConfiguration.baseBackgroundColor = .black.withAlphaComponent(0.2)
+        starButtonConfiguration.image = UIImage(systemName: "star.fill")
+        starButtonConfiguration.imageColorTransformer = UIConfigurationColorTransformer{ _ in .systemYellow }
+        starButtonConfiguration.baseForegroundColor = .white
     }
     
     override func configureHierarchy() {
-        contentView.addSubviews(photoImageView)
+        contentView.addSubviews(photoImageView, starButton)
     }
     
     override func configureConstraints() {
         photoImageView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        starButton.snp.makeConstraints { make in
+            make.bottom.leading.equalToSuperview().inset(8)
+        }
     }
     
     func configure(with value: Photo) {
+        
+        starButtonConfiguration.title = value.likes.decimal()
+        starButton.configuration = starButtonConfiguration
+        
         let height = (CGFloat(value.height) * width) / CGFloat(value.width)
         let size = CGSize(width: width, height: height)
         let url = URL(string: value.urls.raw)
