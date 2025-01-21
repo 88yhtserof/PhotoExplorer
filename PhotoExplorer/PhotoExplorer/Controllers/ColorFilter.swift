d86ce3c42be19c0db5275912e12d23e5967b4be7
88yhtserof <88yhtserofg@gmail.com>
2025-01-21 15:25:52 +0900

    [FEAT] ColorFilter 모델 생성 #1

---
+++ PhotoExplorer/PhotoExplorer/Controllers/ColorFilter.swift
@@ -0,0 +1,59 @@
+//
+//  ColorFilter.swift
+//  PhotoExplorer
+//
+//  Created by 임윤휘 on 1/21/25.
+//
+
+import UIKit
+
+struct ColorFilter {
+    let title: String
+    let color: Color
+    
+    enum Color: String, CaseIterable {
+        case black, white, yellow, red, purple, green, blue
+        
+        var title: String {
+            return rawValue
+        }
+        
+        var title_kr: String {
+            switch self {
+            case .black:
+                return "검정"
+            case .white:
+                return "흰색"
+            case .yellow:
+                return "노랑"
+            case .red:
+                return "빨강"
+            case .purple:
+                return "보라"
+            case .green:
+                return "초록"
+            case .blue:
+                return "파랑"
+            }
+        }
+        
+        var color: UIColor {
+            switch self {
+            case .black:
+                return .black
+            case .white:
+                return .white
+            case .yellow:
+                return .yellow
+            case .red:
+                return .red
+            case .purple:
+                return .purple
+            case .green:
+                return .green
+            case .blue:
+                return .blue
+            }
+        }
+    }
+}
