4c751dc1016d2fe2f725af8e85c01a2ca57aca6a
88yhtserof <88yhtserofg@gmail.com>
2025-01-21 15:23:33 +0900

    [FEAT] 커스텀 ScrollView 구현 #1

---
+++ PhotoExplorer/PhotoExplorer/Views/ScrollView.swift
@@ -0,0 +1,49 @@
+//
+//  ScrollView.swift
+//  PhotoExplorer
+//
+//  Created by 임윤휘 on 1/21/25.
+//
+import UIKit
+import SnapKit
+
+class ScrollView: UIScrollView {
+    
+    enum Direction {
+        case vertical
+        case hotizontal
+    }
+    
+    let scrollDirection: Direction
+    
+    let contentView = UIView()
+    
+    init(_ scrollDirection: Direction = .vertical) {
+        self.scrollDirection = scrollDirection
+        super.init(frame: .zero)
+        
+        configureHierarchy()
+        configureConstraints()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() {
+        addSubviews(contentView)
+    }
+    
+    func configureConstraints() {
+        contentView.snp.makeConstraints { make in
+            switch scrollDirection {
+            case .hotizontal:
+                make.height.equalToSuperview()
+            case .vertical:
+                make.width.equalToSuperview()
+            }
+            make.verticalEdges.equalToSuperview()
+        }
+    }
+}
